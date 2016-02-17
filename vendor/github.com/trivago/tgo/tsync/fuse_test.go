b0c537f6b1666b306b30cc09a4688297981cd360
Arne Claus <arne.claus@trivago.com>
2016-02-17 15:19:22 +0100

    Switch from Godep to govend Added travis CI yml

---
+++ vendor/github.com/trivago/tgo/tsync/fuse_test.go
@@ -0,0 +1,48 @@
+package tsync
+
+import (
+	"github.com/trivago/tgo/ttesting"
+	"testing"
+	"time"
+)
+
+func TestFuse(t *testing.T) {
+	expect := ttesting.NewExpect(t)
+	fuse := NewFuse()
+
+	expect.False(fuse.IsBurned())
+	expect.NonBlocking(5*time.Millisecond, fuse.Wait)
+
+	// Check reactivate of single wait
+
+	start := time.Now()
+	fuse.Burn()
+	expect.True(fuse.IsBurned())
+
+	time.AfterFunc(100*time.Millisecond, fuse.Activate)
+	expect.NonBlocking(300*time.Millisecond, fuse.Wait)
+	expect.False(fuse.IsBurned())
+	expect.Less(int64(time.Since(start)), int64(150*time.Millisecond))
+
+	// Check repeated burning
+
+	fuse.Burn()
+	expect.True(fuse.IsBurned())
+
+	time.AfterFunc(100*time.Millisecond, fuse.Activate)
+	expect.NonBlocking(300*time.Millisecond, fuse.Wait)
+	expect.False(fuse.IsBurned())
+
+	// Check reactivate of multiple waits
+
+	fuse.Burn()
+	expect.True(fuse.IsBurned())
+
+	time.AfterFunc(100*time.Millisecond, fuse.Activate)
+	go expect.NonBlocking(300*time.Millisecond, fuse.Wait)
+	go expect.NonBlocking(300*time.Millisecond, fuse.Wait)
+	go expect.NonBlocking(300*time.Millisecond, fuse.Wait)
+
+	time.Sleep(400 * time.Millisecond)
+	expect.False(fuse.IsBurned())
+}
