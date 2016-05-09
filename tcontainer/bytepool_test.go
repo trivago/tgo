719ae516c06f8fd391371b25485bcf2db0118063
Arne Claus <arne.claus@trivago.com>
2016-05-09 12:16:41 +0200

    Changed bytepool allocation method + new finalizer assignment

--- tcontainer/bytepool_test.go
+++ tcontainer/bytepool_test.go
@@ -26,40 +26,40 @@ func TestBytePool(t *testing.T) {
 	pool := NewBytePool()
 
 	tinyMin := pool.Get(1)
-	expect.Equal(64, cap(tinyMin))
+	expect.Equal(pool.tiny.unit, cap(tinyMin))
 	expect.Equal(1, len(tinyMin))
 
-	tinyMax := pool.Get(960)
-	expect.Equal(960, cap(tinyMax))
-	expect.Equal(960, len(tinyMax))
+	tinyMax := pool.Get(pool.tiny.max)
+	expect.Equal(pool.tiny.max, cap(tinyMax))
+	expect.Equal(pool.tiny.max, len(tinyMax))
 
-	smallMin := pool.Get(961)
-	expect.Equal(1024, cap(smallMin))
-	expect.Equal(961, len(smallMin))
+	smallMin := pool.Get(pool.tiny.max + 1)
+	expect.Equal(pool.small.unit, cap(smallMin))
+	expect.Equal(pool.tiny.max+1, len(smallMin))
 
-	smallMax := pool.Get(1024 * 9)
-	expect.Equal(1024*9, cap(smallMax))
-	expect.Equal(1024*9, len(smallMax))
+	smallMax := pool.Get(pool.small.max)
+	expect.Equal(pool.small.max, cap(smallMax))
+	expect.Equal(pool.small.max, len(smallMax))
 
-	mediumMin := pool.Get(1024*9 + 1)
-	expect.Equal(1024*10, cap(mediumMin))
-	expect.Equal(1024*9+1, len(mediumMin))
+	mediumMin := pool.Get(pool.small.max + 1)
+	expect.Equal(pool.medium.unit, cap(mediumMin))
+	expect.Equal(pool.small.max+1, len(mediumMin))
 
-	mediumMax := pool.Get(1024 * 90)
-	expect.Equal(1024*90, cap(mediumMax))
-	expect.Equal(1024*90, len(mediumMax))
+	mediumMax := pool.Get(pool.medium.max)
+	expect.Equal(pool.medium.max, cap(mediumMax))
+	expect.Equal(pool.medium.max, len(mediumMax))
 
-	largeMin := pool.Get(1024*90 + 1)
-	expect.Equal(1024*100, cap(largeMin))
-	expect.Equal(1024*90+1, len(largeMin))
+	largeMin := pool.Get(pool.medium.max + 1)
+	expect.Equal(pool.large.unit, cap(largeMin))
+	expect.Equal(pool.medium.max+1, len(largeMin))
 
-	largeMax := pool.Get(1024 * 1000)
-	expect.Equal(1024*1000, cap(largeMax))
-	expect.Equal(1024*1000, len(largeMax))
+	largeMax := pool.Get(pool.large.max)
+	expect.Equal(pool.large.max, cap(largeMax))
+	expect.Equal(pool.large.max, len(largeMax))
 
-	huge := pool.Get(1024*1000 + 1)
-	expect.Equal(1024*1000+1, cap(huge))
-	expect.Equal(1024*1000+1, len(huge))
+	huge := pool.Get(pool.large.max + 1)
+	expect.Equal(pool.large.max+1, cap(huge))
+	expect.Equal(pool.large.max+1, len(huge))
 }
 
 func allocateWaste(pool *BytePool, expect ttesting.Expect) {
@@ -68,24 +68,24 @@ func allocateWaste(pool *BytePool, expect ttesting.Expect) {
 		data[i] = byte(i)
 	}
 
-	expect.Equal(0, len(pool.tiny.slabs[0]))
+	expect.Equal(int32(-1), *pool.tiny.slabs[0].top)
 }
 
 func TestBytePoolRecycle(t *testing.T) {
 	expect := ttesting.NewExpect(t)
-	pool := NewBytePoolWithSize(1, 1, 1, 1)
+	pool := NewBytePool()
 
 	expect.Nil(pool.tiny.slabs[0])
 	allocateWaste(&pool, expect)
 
 	expect.NonBlocking(time.Second, func() {
-		for len(pool.tiny.slabs[0]) == 0 {
+		for *pool.tiny.slabs[0].top < 0 {
 			runtime.Gosched()
 			runtime.GC()
 		}
 	})
 
-	expect.Equal(1, len(pool.tiny.slabs[0]))
+	expect.Equal(int32(0), *pool.tiny.slabs[0].top)
 
 	data := pool.Get(32)
 	for i := 0; i < 32; i++ {
