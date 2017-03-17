6c476bd48fb905b24debe865f214a6462f6b182b
Arne Claus <arne.claus@trivago.com>
2017-03-17 22:13:24 +0100

    mend

--- tos/file_unix.go
+++ tos/file_unix.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build cgo,go1.7,!windows,!android
+// +build cgo,go1.7
 
 package tos
 
--- tos/file_unsupported.go
+++ tos/file_unsupported.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !go1.7 !cgo windows android
+// +build !go1.7 !cgo
 
 package tos
 
